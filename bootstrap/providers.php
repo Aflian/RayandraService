c69a198c9ebea201761263eeef28c54893d86b96
Rayandra Dev <dev@rayandra.id>
2026-08-19 19:15:42 +0700

    instalasi panel filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,15 +1,9 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-use App\Providers\Filament\AdminPanelProvider;
-use App\Providers\Filament\CustomerPanelProvider;
-use App\Providers\Filament\InvitationPanelProvider;
-use App\Providers\Filament\WorkspacePanelProvider;
-
 return [
-    AppServiceProvider::class,
-    AdminPanelProvider::class,
-    WorkspacePanelProvider::class,
-    CustomerPanelProvider::class,
-    InvitationPanelProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\CustomerPanelProvider::class,
+    App\Providers\Filament\InvitationPanelProvider::class,
+    App\Providers\Filament\WorkspacePanelProvider::class,
 ];
